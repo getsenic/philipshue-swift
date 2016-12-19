2ff7c38b28e1fd2b035fdfe6ef4af1f6cc5cca64
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2016-12-19 17:16:58 +0100

    Add view controllers to display bridges and lights

---
+++ PhilipsHueDemo/BridgeSelectionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  BridgeSelectionViewController.swift
+//  PhilipsHueDemo
+//
+//  Created by Lars Blumberg on 12/19/16.
+//  Copyright © 2016 senic. All rights reserved.
+//
+
+import UIKit
+
+class BridgeSelectionViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var bridges: [PhilipsHueBridge] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        discoverBridges()
+    }
+
+    @IBAction func discoverBridges() {
+        //TODO: Implement discovery
+        bridges = [PhilipsHueBridge(host: "192.168.1.83", username: "WuXoBjbkWhR4rxpmrLgkdEvAZ0JKPb7f6Rl0wV-D")]
+        tableView.reloadData()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        (segue.destination as! BridgeViewController).bridge = sender as! PhilipsHueBridge
+    }
+}
+
+extension BridgeSelectionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bridges.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bridge", for: indexPath)
+        cell.textLabel?.text = bridges[indexPath.row].host
+        return cell
+    }
+}
+
+extension BridgeSelectionViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "bridge", sender: bridges[indexPath.row])
+    }
+}
---
+++ PhilipsHueDemo/BridgeViewController.swift
@@ -0,0 +1,71 @@
+//
+//  BridgeViewController.swift
+//  PhilipsHueDemo
+//
+//  Created by Lars Blumberg on 12/19/16.
+//  Copyright © 2016 senic. All rights reserved.
+//
+
+import UIKit
+
+class BridgeViewController: UIViewController {
+    var bridge: PhilipsHueBridge!
+
+    var groups: [PhilipsHueGroup] { return bridge.groups.values.sorted(by: { (lhs, rhs) -> Bool in return lhs.identifier < rhs.identifier }) }
+
+    var lights: [PhilipsHueLight] { return bridge.lights.values.sorted(by: { (lhs, rhs) -> Bool in return lhs.identifier < rhs.identifier }) }
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = bridge.host
+
+        refresh()
+    }
+
+    @IBAction func refresh() {
+        bridge.refresh { [weak self] (result) in
+            switch result {
+            case .failure(let error): print(error)
+            case .success(): self?.tableView.reloadData()
+            }
+        }
+    }
+}
+
+extension BridgeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? bridge.groups.count : bridge.lights.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let group = groups[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "group", for: indexPath)
+            cell.textLabel?.text = group.identifier
+            cell.detailTextLabel?.text = String(describing: group.type)
+            return cell
+        }
+        else {
+            let light = lights[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "light", for: indexPath)
+            cell.textLabel?.text = light.identifier
+            cell.detailTextLabel?.text = ""
+            return cell
+        }
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "Groups" : "Lights"
+    }
+}
+
+extension BridgeViewController: UITableViewDelegate {
+
+}
