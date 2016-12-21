1ca1925935ce7abff33aae8250230399caff39ed
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2016-12-21 13:23:39 +0100

    Notify “parameter not available” errors

--- PhilipsHue/PhilipsHueResult.swift
+++ PhilipsHue/PhilipsHueResult.swift
@@ -12,6 +12,7 @@ public enum PhilipsHueError: Error {
     case usernameNotSet
     case unauthorizedUser
     case resourceNotAvailable
+    case parameterNotAvailable
     case linkButtonNotPressed
     case lightIsOff
     case groupTableFull
@@ -23,6 +24,7 @@ public enum PhilipsHueError: Error {
         switch code {
         case   1: self = .unauthorizedUser
         case   3: self = .resourceNotAvailable
+        case   6: self = .parameterNotAvailable
         case 101: self = .linkButtonNotPressed
         case 201: self = .lightIsOff
         case 301: self = .groupTableFull
@@ -37,6 +39,7 @@ extension PhilipsHueError: LocalizedError {
         case .usernameNotSet:                   return "Username not set"
         case .unauthorizedUser:                 return "User not authorized"
         case .resourceNotAvailable:             return "Resource not available"
+        case .parameterNotAvailable:            return "Parameter not available"
         case .linkButtonNotPressed:             return "Link Button not pressed"
         case .lightIsOff:                       return "Light is off"
         case .groupTableFull:                   return "Cannot create group, group table already full"
